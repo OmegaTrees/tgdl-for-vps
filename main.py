40127190eeeae5b0f5f03d472485244d0ecd2832
OmegaTrees <101999535+OmegaTrees@users.noreply.github.com>
2025-11-01 13:32:49 +0000

    Refactor bot initialization and remove old code

--- main.py
+++ main.py
@@ -8,70 +8,14 @@
 # @markdown <center><h4><a href="https://github.com/XronTrix10/Telegram-Leecher/wiki/INSTRUCTIONS">READ</a><b> How to use</h4></b></center>
 # @markdown <br><center><h2><font color=lime><strong>Fill all Credentials, Run The Cell and Start The Bot</strong></h2></center>
 # @markdown <br><br>
+from colab_leecher import __main__ as bot_main
+
+
 
 API_ID = 0  # @param {type: "integer"}
 API_HASH = ""  # @param {type: "string"}
 BOT_TOKEN = ""  # @param {type: "string"}
 USER_ID = 0  # @param {type: "integer"}
 DUMP_ID = 0  # @param {type: "integer"}
-
-import subprocess, time, json, shutil, os
-from IPython.display import clear_output, display, HTML
-from threading import Thread
-
-Working = True
-
-def keep_alive(url):
-    display(HTML(f'<audio src="{url}" controls autoplay style="display:none"></audio>'))
-
-def Loading():
-    white = 37
-    black = 0
-    while Working:
-        print("\r" + "░"*white + "▒▒"+ "▓"*black + "▒▒" + "░"*white, end="")
-        black = (black + 2) % 75
-        white = (white -1) if white != 0 else 37
-        time.sleep(2)
-    clear_output()
-
-audio_url    = "https://raw.githubusercontent.com/KoboldAI/KoboldAI-Client/main/colab/silence.m4a"
-audio_thread = Thread(target=keep_alive, args=(audio_url,))
-audio_thread.start()
-_Thread = Thread(target=Loading, name="Prepare", args=())
-_Thread.start()
-
-if len(str(DUMP_ID)) == 10 and "-100" not in str(DUMP_ID):
-    n_dump = "-100" + str(DUMP_ID)
-    DUMP_ID = int(n_dump)
-
-if os.path.exists("/content/sample_data"):
-    shutil.rmtree("/content/sample_data")
-
-if os.path.exists("/content/tgdl"):
-    shutil.rmtree("/content/tgdl")
-
-cmd = "git clone https://github.com/ehraz786/tgdl
-proc = subprocess.run(cmd, shell=True)
-cmd = "apt update && apt install ffmpeg aria2 megatools"
-proc = subprocess.run(cmd, shell=True)
-cmd = "pip3 install -r /content/tgdl/requirements.txt"
-proc = subprocess.run(cmd, shell=True)
-
-credentials = {
-    "API_ID": API_ID,
-    "API_HASH": API_HASH,
-    "BOT_TOKEN": BOT_TOKEN,
-    "USER_ID": USER_ID,
-    "DUMP_ID": DUMP_ID,
-}
-
-with open('/content/tgdl/credentials.json', 'w') as file:
-    file.write(json.dumps(credentials))
-
-Working = False
-
-if os.path.exists("/content/tgdl/my_bot.session"):
-    os.remove("/content/tgdl/my_bot.session") # Remove previous bot session
-print("\rStarting Bot....")
-
-!cd /content/tgdl/ && python3 -m colab_leecher #type:ignore
+# Pass them to __main__.py
+bot_main.start_bot(API_ID, API_HASH, BOT_TOKEN, OWNER, DUMP_ID)
